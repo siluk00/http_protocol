342baf9d967dd77f87e0059f3ed1ca867e18b3b9
siluk00 <sil-uk@hotmail.com>
2026-04-04 14:20:13 -0300

    multiple headers

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -32,4 +32,23 @@ func TestParse(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	headers = NewHeaders()
+	data = []byte("Host:  localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 24, n)
+	assert.False(t, done)
+
+	headers = NewHeaders()
+	headers["set-person"] = "Me-loves-nun"
+	data = []byte("Set-Person: lane-loves-go\r\nSet-Person: prime-loves-zig\r\nSet-Person: tj-loves-ocaml;\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "Me-loves-nun, lane-loves-go", headers["set-person"])
+	assert.Equal(t, 27, n)
+	assert.False(t, done)
 }
